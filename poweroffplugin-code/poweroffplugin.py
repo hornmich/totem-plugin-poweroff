05d2eb0b6cfbd2edcf3f75a7e2d7a7f169a0c767
Michal Horn <michal@michal-desktop.(none)>
2012-04-08 22:27:24 +0200

    Done procedure for suspend Done procedure for power off Done first attempt to radio button state check

--- poweroffplugin-code/poweroffplugin.py
+++ poweroffplugin-code/poweroffplugin.py
@@ -9,6 +9,7 @@ import threading
 import time
 import urllib
 import urllib2
+import subprocess
 from xml.sax.saxutils import escape
 
 class poweroffplugin(totem.Plugin):
@@ -20,8 +21,13 @@ class poweroffplugin(totem.Plugin):
         self.configDialog = self.builder.get_object('config_dialog')
         self.radioUseDBus = self.builder.get_object('radioUseDBus')
         self.radioOwnComm = self.builder.get_object('radioOwnComm')
-        self.entryShutDown = self.builder.get_object('entryShutDown')
+        self.radioPowerOff = self.builder.get_object('radioTurnOff')
+	self.radioSuspend = self.builder.get_object('radioSuspend')
+	self.radioDoNothing = self.builder.get_object('radioDoNothing')
+	self.entryShutDown = self.builder.get_object('entryShutDown')
         self.entrySuspend = self.builder.get_object('entrySuspend')
+	self.goButton = self.builder.get_object('go_button')
+	self.goButton.connect('clicked', self.goButtonClicked)
         
         self.totem = totem_object
         self.container.show_all();
@@ -31,4 +37,19 @@ class poweroffplugin(totem.Plugin):
     def create_configure_dialog(self, *args):
         self.configDialog.set_default_response(gtk.RESPONSE_OK)
         return self.configDialog
+    def turnOff(self):
+	pid = subprocess.Popen('dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).pid
+    def suspend(self):
+	pid = subprocess.Popen('dbus-send --system --print-reply --dest="org.freedesktop.UPower" /org/freedesktop/UPower org.freedesktop.UPower.Suspend', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).pid
+    def goButtonClicked(self, *args):
+	if self.radioDoNothing.get_active() == True :
+		print "Doing nothing\n"
+	elif self.radioSuspend.get_active() == True :
+		print "Suspending\n"
+	elif self.radioPowerOff.get_active() == True :
+		print "Power off\n"
+	else :
+		print "error\n"
+	
+
         
